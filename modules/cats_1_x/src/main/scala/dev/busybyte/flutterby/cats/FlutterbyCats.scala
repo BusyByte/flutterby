78e8aef8f58a04a1dd3a6b759b8aab64486cee14
Shawn Garner <shawn.garner@banno.com>
2020-04-20 08:42:46 -0500

    rename to prevent conflicts in namespace

---
+++ modules/cats_1_x/src/main/scala/dev/busybyte/flutterby/cats/FlutterbyCats.scala
@@ -0,0 +1,15 @@
+package dev.busybyte.flutterby.cats
+
+import cats.effect.Sync
+import dev.busybyte.flutterby.core.{ Flutterby, MigrationInfoService }
+
+object FlutterbyCats {
+  def apply[F[_]: Sync]: Flutterby[F] = new Flutterby[F] {
+    override def baseline: F[Unit]                = ???
+    override def migrate: F[Int]                  = ???
+    override def info: F[MigrationInfoService[F]] = ???
+    override def validate(): F[Unit]              = ???
+    override def repair(): F[Unit]                = ???
+    override def clean(): F[Unit]                 = ???
+  }
+}
