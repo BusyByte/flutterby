43e3267cb53770ba09cab28f004cde140804805f
Shawn Garner <shawn.garner@banno.com>
2020-05-02 13:38:52 -0500

    implemented some calback conversions

--- modules/core/src/main/scala/flutterby/core/callback/Callbacks.scala
+++ modules/core/src/main/scala/flutterby/core/callback/Callbacks.scala
@@ -4,13 +4,14 @@ import java.sql.Connection
 
 import flutterby.core.MigrationInfo
 import flutterby.core.config.FlutterbyConfig
+import flutterby.core.jdk.CollectionConversions
 import org.flywaydb.core.api.callback.{
-  Warning => FlywayWarning,
-  Error => FlywayError,
-  Statement => FlywayStatement,
   Callback => FWCallback,
   Context => FlywayContext,
-  Event => FlywayEvent
+  Error => FlywayError,
+  Event => FlywayEvent,
+  Statement => FlywayStatement,
+  Warning => FlywayWarning
 }
 
 trait Callback         {
@@ -53,13 +54,25 @@ sealed trait Statement {
   def getErrors: List[Error]
 }
 object Statement       {
-  def fromFlyway(f: FlywayStatement): Statement = ??? // TODO implement
+  def fromFlyway(f: FlywayStatement): Statement =
+    new Statement {
+      override def getSql: Sql                = Sql(f.getSql)
+      override def getWarnings: List[Warning] = CollectionConversions.toScalaList(f.getWarnings).map(Warning.fromFlyway)
+      override def getErrors: List[Error]     = CollectionConversions.toScalaList(f.getErrors).map(Error.fromFlyway)
+    }
 }
 
 sealed trait `IsHandled?`
 object `IsHandled?`    {
   case object Handled    extends `IsHandled?`
   case object NotHandled extends `IsHandled?`
+
+  def fromFlyway(handled: Boolean): `IsHandled?` = if (handled) Handled else NotHandled
+  def toFlyway(handled: `IsHandled?`): Boolean   =
+    handled match {
+      case Handled    => true
+      case NotHandled => false
+    }
 }
 
 final case class Code(value: Int)       extends AnyVal
@@ -73,8 +86,14 @@ trait Warning          {
   def markHandled(handled: `IsHandled?`): Unit
 }
 object Warning         {
-  def toFlyway(e: Warning): FlywayWarning   = ??? // TODO: implement
-  def fromFlyway(f: FlywayWarning): Warning = ??? // TODO: implement
+  def fromFlyway(f: FlywayWarning): Warning =
+    new Warning {
+      override def code: Code                               = Code(f.getCode)
+      override def state: State                             = State(f.getState)
+      override def message: Message                         = Message(f.getMessage)
+      override def handled: `IsHandled?`                    = `IsHandled?`.fromFlyway(f.isHandled)
+      override def markHandled(handled: `IsHandled?`): Unit = f.setHandled(`IsHandled?`.toFlyway(handled))
+    }
 }
 
 trait Error            {
@@ -86,8 +105,14 @@ trait Error            {
 }
 
 object Error {
-  def toFlyway(e: Error): FlywayError   = ??? // TODO: implement
-  def fromFlyway(f: FlywayError): Error = ??? // TODO: implement
+  def fromFlyway(f: FlywayError): Error =
+    new Error {
+      override def code: Code                               = Code(f.getCode)
+      override def state: State                             = State(f.getState)
+      override def message: Message                         = Message(f.getMessage)
+      override def handled: `IsHandled?`                    = `IsHandled?`.fromFlyway(f.isHandled)
+      override def markHandled(handled: `IsHandled?`): Unit = f.setHandled(`IsHandled?`.toFlyway(handled))
+    }
 }
 
 sealed abstract class Event(val id: String)
@@ -126,8 +151,78 @@ object Event {
   case object AFTER_INFO                         extends Event("afterInfo")
   case object AFTER_INFO_ERROR                   extends Event("afterInfoError")
 
-  def fromFlyway(f: FlywayEvent): Event = ??? // TODO implement
-  def toFlyway(e: Event): FlywayEvent   = ??? // TODO implement
+  def fromFlyway(f: FlywayEvent): Event =
+    f match {
+      case FlywayEvent.BEFORE_CLEAN                       => BEFORE_CLEAN
+      case FlywayEvent.AFTER_CLEAN                        => AFTER_CLEAN
+      case FlywayEvent.AFTER_CLEAN_ERROR                  => AFTER_CLEAN_ERROR
+      case FlywayEvent.BEFORE_MIGRATE                     => BEFORE_MIGRATE
+      case FlywayEvent.BEFORE_EACH_MIGRATE                => BEFORE_EACH_MIGRATE
+      case FlywayEvent.BEFORE_EACH_MIGRATE_STATEMENT      => BEFORE_EACH_MIGRATE_STATEMENT
+      case FlywayEvent.AFTER_EACH_MIGRATE_STATEMENT       => AFTER_EACH_MIGRATE_STATEMENT
+      case FlywayEvent.AFTER_EACH_MIGRATE_STATEMENT_ERROR => AFTER_EACH_MIGRATE_STATEMENT_ERROR
+      case FlywayEvent.AFTER_EACH_MIGRATE                 => AFTER_EACH_MIGRATE
+      case FlywayEvent.AFTER_EACH_MIGRATE_ERROR           => AFTER_EACH_MIGRATE_ERROR
+      case FlywayEvent.AFTER_MIGRATE                      => AFTER_MIGRATE
+      case FlywayEvent.AFTER_MIGRATE_ERROR                => AFTER_MIGRATE_ERROR
+      case FlywayEvent.BEFORE_UNDO                        => BEFORE_UNDO
+      case FlywayEvent.BEFORE_EACH_UNDO                   => BEFORE_EACH_UNDO
+      case FlywayEvent.BEFORE_EACH_UNDO_STATEMENT         => BEFORE_EACH_UNDO_STATEMENT
+      case FlywayEvent.AFTER_EACH_UNDO_STATEMENT          => AFTER_EACH_UNDO_STATEMENT
+      case FlywayEvent.AFTER_EACH_UNDO_STATEMENT_ERROR    => AFTER_EACH_UNDO_STATEMENT_ERROR
+      case FlywayEvent.AFTER_EACH_UNDO                    => AFTER_EACH_UNDO
+      case FlywayEvent.AFTER_EACH_UNDO_ERROR              => AFTER_EACH_UNDO_ERROR
+      case FlywayEvent.AFTER_UNDO                         => AFTER_UNDO
+      case FlywayEvent.AFTER_UNDO_ERROR                   => AFTER_UNDO_ERROR
+      case FlywayEvent.BEFORE_VALIDATE                    => BEFORE_VALIDATE
+      case FlywayEvent.AFTER_VALIDATE                     => AFTER_VALIDATE
+      case FlywayEvent.AFTER_VALIDATE_ERROR               => AFTER_VALIDATE_ERROR
+      case FlywayEvent.BEFORE_BASELINE                    => BEFORE_BASELINE
+      case FlywayEvent.AFTER_BASELINE                     => AFTER_BASELINE
+      case FlywayEvent.AFTER_BASELINE_ERROR               => AFTER_BASELINE_ERROR
+      case FlywayEvent.BEFORE_REPAIR                      => BEFORE_REPAIR
+      case FlywayEvent.AFTER_REPAIR                       => AFTER_REPAIR
+      case FlywayEvent.AFTER_REPAIR_ERROR                 => AFTER_REPAIR_ERROR
+      case FlywayEvent.BEFORE_INFO                        => BEFORE_INFO
+      case FlywayEvent.AFTER_INFO                         => AFTER_INFO
+      case FlywayEvent.AFTER_INFO_ERROR                   => AFTER_INFO_ERROR
+    }
+  def toFlyway(e: Event): FlywayEvent   =
+    e match {
+      case BEFORE_CLEAN                       => FlywayEvent.BEFORE_CLEAN
+      case AFTER_CLEAN                        => FlywayEvent.AFTER_CLEAN
+      case AFTER_CLEAN_ERROR                  => FlywayEvent.AFTER_CLEAN_ERROR
+      case BEFORE_MIGRATE                     => FlywayEvent.BEFORE_MIGRATE
+      case BEFORE_EACH_MIGRATE                => FlywayEvent.BEFORE_EACH_MIGRATE
+      case BEFORE_EACH_MIGRATE_STATEMENT      => FlywayEvent.BEFORE_EACH_MIGRATE_STATEMENT
+      case AFTER_EACH_MIGRATE_STATEMENT       => FlywayEvent.AFTER_EACH_MIGRATE_STATEMENT
+      case AFTER_EACH_MIGRATE_STATEMENT_ERROR => FlywayEvent.AFTER_EACH_MIGRATE_STATEMENT_ERROR
+      case AFTER_EACH_MIGRATE                 => FlywayEvent.AFTER_EACH_MIGRATE
+      case AFTER_EACH_MIGRATE_ERROR           => FlywayEvent.AFTER_EACH_MIGRATE_ERROR
+      case AFTER_MIGRATE                      => FlywayEvent.AFTER_MIGRATE
+      case AFTER_MIGRATE_ERROR                => FlywayEvent.AFTER_MIGRATE_ERROR
+      case BEFORE_UNDO                        => FlywayEvent.BEFORE_UNDO
+      case BEFORE_EACH_UNDO                   => FlywayEvent.BEFORE_EACH_UNDO
+      case BEFORE_EACH_UNDO_STATEMENT         => FlywayEvent.BEFORE_EACH_UNDO_STATEMENT
+      case AFTER_EACH_UNDO_STATEMENT          => FlywayEvent.AFTER_EACH_UNDO_STATEMENT
+      case AFTER_EACH_UNDO_STATEMENT_ERROR    => FlywayEvent.AFTER_EACH_UNDO_STATEMENT_ERROR
+      case AFTER_EACH_UNDO                    => FlywayEvent.AFTER_EACH_UNDO
+      case AFTER_EACH_UNDO_ERROR              => FlywayEvent.AFTER_EACH_UNDO_ERROR
+      case AFTER_UNDO                         => FlywayEvent.AFTER_UNDO
+      case AFTER_UNDO_ERROR                   => FlywayEvent.AFTER_UNDO_ERROR
+      case BEFORE_VALIDATE                    => FlywayEvent.BEFORE_VALIDATE
+      case AFTER_VALIDATE                     => FlywayEvent.AFTER_VALIDATE
+      case AFTER_VALIDATE_ERROR               => FlywayEvent.AFTER_VALIDATE_ERROR
+      case BEFORE_BASELINE                    => FlywayEvent.BEFORE_BASELINE
+      case AFTER_BASELINE                     => FlywayEvent.AFTER_BASELINE
+      case AFTER_BASELINE_ERROR               => FlywayEvent.AFTER_BASELINE_ERROR
+      case BEFORE_REPAIR                      => FlywayEvent.BEFORE_REPAIR
+      case AFTER_REPAIR                       => FlywayEvent.AFTER_REPAIR
+      case AFTER_REPAIR_ERROR                 => FlywayEvent.AFTER_REPAIR_ERROR
+      case BEFORE_INFO                        => FlywayEvent.BEFORE_INFO
+      case AFTER_INFO                         => FlywayEvent.AFTER_INFO
+      case AFTER_INFO_ERROR                   => FlywayEvent.AFTER_INFO_ERROR
+    }
 }
 
 final case class Callbacks(callbacks: Vector[Callback])
