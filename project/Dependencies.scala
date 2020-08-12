5e91a6773eb9e07b6c00f51355975f536e6bc6bf
Scala Steward <me@scala-steward.org>
2020-08-12 16:19:54 +0200

    Update flyway-core to 6.5.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     // Compiler
     val kindProjector    = "0.10.3"
     val betterMonadicFor = "0.3.1"
-    val flyway           = "6.5.3"
+    val flyway           = "6.5.4"
   }
 
   object Libraries {
