7eaf98a8bea692b82839444d1bb1db2b242d425a
Scala Steward <me@scala-steward.org>
2020-11-06 12:16:39 +0100

    Update postgresql to 1.15.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     // Test
     val specs2                        = "4.10.5"
     val testContainersScalaVersion    = "0.38.4" // https://github.com/testcontainers/testcontainers-scala/releases
-    val testContainersPostgresVersion = "1.14.3" // https://github.com/testcontainers/testcontainers-java/releases
+    val testContainersPostgresVersion = "1.15.0" // https://github.com/testcontainers/testcontainers-java/releases
     val postgresVersion               = "42.2.18"
 
     // Compiler
